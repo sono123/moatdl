10614299845f4e7f056997aeee887bf14aadb76c
steveono <steveono@gmail.com>
2015-08-14 00:29:45 -0400

    Add Rails Dom Id Helper.

--- spec/support/todo_list_helpers.rb
+++ spec/support/todo_list_helpers.rb
@@ -1,8 +1,9 @@
 module TodoListHelpers
 	def visit_todo_list(list)
 		visit "/todo_lists"
-		within "#todo_list_#{list.id}" do
+		within dom_id_for(list) do ### same as "#todo_list_#{list.id}"
 			click_link "List Items"
 		end
 	end
 end
+
