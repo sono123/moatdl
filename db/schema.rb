c19e3c0c3b1c1dc12049584bf8f80c5e6791ac81
steveono <steveono@gmail.com>
2015-08-20 23:40:35 -0400

    Add migration add_password_reset_token_to_users and implement the method in the user model, along with specs.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820033240) do
+ActiveRecord::Schema.define(version: 20150821031846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,8 +43,10 @@ ActiveRecord::Schema.define(version: 20150820033240) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_reset_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["password_reset_token"], name: "index_users_on_password_reset_token", using: :btree
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -56,6 +56,19 @@ RSpec.describe User, type: :model do
   		end
   	end
 
+    context "#generate_password_reset_token!" do
+      let(:user) { create(:user) }
+
+      it "changes the password_reset_token attribute" do
+        expect{ user.generate_password_reset_token! }.to change{user.password_reset_token}
+      end
+
+      it "calls SecureRandom.urlsafe_base64 to generate the password_reset_token" do
+        expect(SecureRandom).to receive(:urlsafe_base64)
+        user.generate_password_reset_token!
+      end
+    end
+
   end
 
 end
