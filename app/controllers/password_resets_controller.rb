cb4fa8d0db1d964dd300adb1d842e913615c713b
steveono <steveono@gmail.com>
2015-08-21 23:27:21 -0400

    Fixed password_reset_token! method and adjusted users_controller to redirect and set flash message correctly.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,8 +4,14 @@ class PasswordResetsController < ApplicationController
 
 	def create
 		user = User.find_by(email: params[:email])
-		user.generate_password_reset_token!
-		Notifier.password_reset(user).deliver
-		redirect_to login_path
+		if user
+			user.generate_password_reset_token!
+			Notifier.password_reset(user).deliver
+			flash[:success] = "Please check your email to reset password."
+			redirect_to login_path
+		else
+			flash.now[:notice] = "Email not found."
+			render action: "new"
+		end
 	end
 end
--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -24,6 +24,29 @@ RSpec.describe PasswordResetsController, type: :controller do
 			it "sends a password reset email" do
 				expect{ post :create, email: user.email }.to change(ActionMailer::Base.deliveries, :size)
 			end
+
+			it "sets the flash success message" do
+				post :create, email: user.email
+				expect(flash[:success]).to match(/check your email/)
+			end
+		end
+
+		context "with no user found" do
+			it "renders the new page" do
+				post :create, email: "none@found.com"
+				expect(response).to render_template("new")
+			end
+
+			it "sets the flash message" do
+				post :create, email: "none@found.com"
+				expect(flash[:notice]).to match(/not found/)
+			end
 		end
 	end
 end
+
+
+
+
+
+
